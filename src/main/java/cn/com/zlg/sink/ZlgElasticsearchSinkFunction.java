6005e56e9a9b89f35efe057d110ebee7081f10dc
zhaoxin <187182519@qq.com>
2019-08-02 21:21:05 +0800

    first code

---
+++ src/main/java/cn/com/zlg/sink/ZlgElasticsearchSinkFunction.java
@@ -0,0 +1,22 @@
+package cn.com.zlg.sink;
+
+import cn.com.zlg.util.ZlgElasticsearchUtil;
+import org.apache.flink.api.common.functions.RuntimeContext;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.connectors.elasticsearch.ElasticsearchSinkFunction;
+import org.apache.flink.streaming.connectors.elasticsearch.RequestIndexer;
+
+
+public class ZlgElasticsearchSinkFunction implements ElasticsearchSinkFunction {
+
+    @Override
+    public void process(Object o, RuntimeContext runtimeContext, RequestIndexer requestIndexer) {
+        ZlgElasticsearchUtil esUtil = new ZlgElasticsearchUtil();
+        Tuple2 temp = (Tuple2)o;
+        try {
+            requestIndexer.add(esUtil.addSource("hello","id-" + temp.f0,temp));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
